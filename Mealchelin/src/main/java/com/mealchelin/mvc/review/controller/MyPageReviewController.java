a41fcdd50ef2d405106c391a8bdc6151c6fea000
DESKTOP-835L1KV\user1 <eunmi080163@gmail.com>
2024-03-04 16:48:10 +0900

    no message

--- Mealchelin/src/main/java/com/mealchelin/mvc/review/controller/MyPageReviewController.java
+++ Mealchelin/src/main/java/com/mealchelin/mvc/review/controller/MyPageReviewController.java
@@ -13,7 +13,7 @@ import lombok.extern.slf4j.Slf4j;
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/mypage")
-public class myPageReviewController {
+public class MyPageReviewController {
 	@GetMapping("/productReview")
 	public String mypageProductReview() {
 		
