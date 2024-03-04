5b95ce72086d48ac3e38bba749cd8aa368c4525a
JunSeok <eunmi080163@gmail.com>
2024-03-04 17:37:51 +0900

    준석_ 금요일까지 했던 내용들 복구 완료(세부적인 매핑주소는 수정필요함)

--- Mealchelin/src/main/java/com/mealchelin/mvc/review/controller/MyPageReviewController.java
+++ Mealchelin/src/main/java/com/mealchelin/mvc/review/controller/MyPageReviewController.java
@@ -1,10 +1,17 @@
 package com.mealchelin.mvc.review.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 
+import com.mealchelin.mvc.common.util.PageInfo;
+import com.mealchelin.mvc.review.model.mapper.ReviewMapper;
 import com.mealchelin.mvc.review.model.service.ReviewService;
+import com.mealchelin.mvc.review.model.vo.Review;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -14,12 +21,37 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 @RequestMapping("/mypage")
 public class MyPageReviewController {
-	@GetMapping("/productReview")
-	public String mypageProductReview() {
+	private ReviewMapper reviewMapper;
+	
+	private final ReviewService service;
+	
+	@GetMapping("/mypageProductReview")
+	public ModelAndView list(ModelAndView modelAndView, @RequestParam(defaultValue = "1") int page) {
+
+		int reviewCount = 0;
+		PageInfo pageInfo = null;
+		List<Review> list = null;
 		
+		log.info("============================");
+		log.info(modelAndView.toString());
+		log.info("============================");
+
+		reviewCount = service.getReviewCount();
+		pageInfo = new PageInfo(page, 5, reviewCount, 5);
+		list = service.getReviewList(pageInfo);
+
 		
-        return "mypage/productReview";
-    }
+		log.info("============================");
+		log.info("page Number : {}", page);
+		log.info("List Count : {}", reviewCount);
+		log.info("============================");
+
+		modelAndView.addObject("pageInfo", pageInfo);
+		modelAndView.addObject("list", list);
+		modelAndView.setViewName("review/MypageProductReview");
+
+		return modelAndView;
+	}
 	
 	
 	@GetMapping("/writableReview")
