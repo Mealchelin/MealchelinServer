e56db8bac18f66fc9b3b875ca4787687bb12e292
yeonhee <shfwy123@naver.com>
2024-03-05 21:13:26 +0900

    0305 학원 커밋

--- Mealchelin/src/main/java/com/mealchelin/mvc/member/controller/MemberController.java
+++ Mealchelin/src/main/java/com/mealchelin/mvc/member/controller/MemberController.java
@@ -46,7 +46,10 @@ public class MemberController {
 		Member loginMember = service.login(id,password);
 		
 		if (loginMember != null) {
+			String adminYN = loginMember.getRole();
+	        log.info(adminYN);
 			modelAndView.addObject("loginMember",loginMember);
+			modelAndView.addObject("adminYN",adminYN);
 			modelAndView.setViewName("redirect:/");	
 		} else {
 			modelAndView.addObject("msg", "아이디나 비밀번호가 일치하지 않습니다.");
