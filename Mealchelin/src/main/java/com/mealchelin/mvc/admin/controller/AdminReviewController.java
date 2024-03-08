ac54a8407795ff3e6341af10cdda8905ba73da54
JunSeok <eunmi080163@gmail.com>
2024-03-08 17:43:59 +0900

    준석_어드민리뷰 리뷰목록 불러오기 성공

--- Mealchelin/src/main/java/com/mealchelin/mvc/admin/controller/AdminReviewController.java
+++ Mealchelin/src/main/java/com/mealchelin/mvc/admin/controller/AdminReviewController.java
@@ -1,15 +1,50 @@
 package com.mealchelin.mvc.admin.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.mealchelin.mvc.common.util.PageInfo;
+import com.mealchelin.mvc.review.model.mapper.ReviewMapper;
+import com.mealchelin.mvc.review.model.service.ReviewService;
+import com.mealchelin.mvc.review.model.vo.Review;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
+@RequiredArgsConstructor
 @RequestMapping("/admin/review")
 public class AdminReviewController {
+	private ReviewMapper reviewMapper;
+	
+	private final ReviewService service;
+	
+	
 	@GetMapping("/adReview")
-	public ModelAndView adReview(ModelAndView modelAndView) {
+	public ModelAndView adReview(ModelAndView modelAndView, @RequestParam(defaultValue = "1") int page) {
+		int reviewCount = 0;
+		PageInfo pageInfo = null;
+		List<Review> list = null;
+		
+		log.info(modelAndView.toString());
+
+		reviewCount = service.getReviewCount();
+		pageInfo = new PageInfo(page, 5, reviewCount, 12);
+		list = service.getReviewList(pageInfo);
+
+		
+		log.info("page Number : {}", page);
+		log.info("List Count : {}", reviewCount);
+		log.info("============================");
+
+		modelAndView.addObject("pageInfo", pageInfo);
+		modelAndView.addObject("list", list);
 		
 		modelAndView.setViewName("admin/review/adReview");
 		
